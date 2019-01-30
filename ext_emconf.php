d0b888e4c5e4f63c69ef80103ac985f0fe2d416b
m.chubin <mxmchbn@gmail.com>
2019-01-30 15:10:33 +0200

    [FEATURE] TYPO3 9.5 Support

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '1',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '9.5.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
